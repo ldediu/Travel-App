0717c767125c43edb546e89157ea9047aee02ecc
Leonid Dediu <mdleonidas@gmail.com>
2020-09-09 22:29:15 -0700

    started working on client side

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,3 +1,5 @@
+import { updateUI } from './updateUI';
+
 async function main(event) {
     event.preventDefault();
 
@@ -5,6 +7,9 @@ async function main(event) {
     let destination_city = document.getElementById('destination').value;
     let dep_time = document.getElementById('date_from').value;
     let ret_time = document.getElementById('date_return').value;
+    let dyn_cont = document.getElementById('dyn_all');
+
+    let newCard;
 
     const api_response = await fetch("/results", {
         method: "POST",
@@ -19,6 +24,8 @@ async function main(event) {
     try {
         const data = await api_response.json();
         console.log('client = ', data);
+        newCard = updateUI(data);
+        dyn_cont.appendChild(newCard);
     } catch (error) {
         console.log('error', error);
     }
